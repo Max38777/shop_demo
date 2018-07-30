21e486ea7ff29f0596f8f1895e91fe93a18b9aee
Max38777 <writemenow10@gmail.com>
2018-07-31 00:38:46 +0300

    Task 3: ProductForm, Registration, Login, OrderForm(validation)

--- shop_app/models.py
+++ shop_app/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+from django.conf import settings
 
 # Создаем базовую модель нашего продукта
 class Product(models.Model): 
@@ -9,9 +11,19 @@ class Product(models.Model):
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self): 
+      return reverse('detail_product', args=[str(self.id)])
+
 class Category(models.Model): 
     title = models.CharField(max_length=200)
     description = models.TextField(max_length=5000, blank=True)
 
     def __str__(self):
         return self.title
+
+class Order(models.Model): 
+    product = models.ForeignKey(Product, on_delete='CASCADE') 
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete='CASCADE',null=True)
+    customer_name = models.CharField(max_length=200)
+    customer_phone = models.CharField(max_length=200)
+    
--- shop_app/views.py
+++ shop_app/views.py
@@ -1,8 +1,11 @@
 # импортируем модель для CBV            
 from django.views import generic 
-
 # импортируем нашу модель
-from .models import Product,Category
+from .models import Product,Category,Order
+from django.conf.urls import include
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class MainView(generic.ListView):
     
@@ -31,3 +34,45 @@ class CategoryDetail(generic.DetailView):
 class ProductDetail(generic.DetailView): 
     template_name = 'product_detail.html' 
     model = Product
+
+class ProductCreate(generic.CreateView): 
+	model = Product 
+	# название нашего шаблона с формой
+	template_name = 'product_new.html' 
+	# какие поля будут в форме 
+	fields = '__all__'
+
+class OrderFormView(LoginRequiredMixin, generic.CreateView): 
+    model = Order 
+    template_name = 'order_form.html' 
+    success_url = '/' 
+    #fields = '__all__'
+    fields = ['customer_name', 'customer_phone']
+  
+    def get_initial(self):
+        first_name = self.request.user.first_name
+        return {'customer_name': first_name}
+
+    def form_valid(self, form):
+        # получаем ID из ссылки и передаем в ORM для фильтрации
+        product = Product.objects.get(id=self.kwargs['pk']) 
+        # передаем в поле товара нашей формы отфильтрованный товар
+        form.instance.product = product 
+        # получаем пользователя из сессии
+        user = self.request.user
+        # передаем в поле формы
+        form.instance.user = user
+        # super — перезагружает форму, нужен для работы
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs): 
+        context = super().get_context_data(**kwargs)
+        product = Product.objects.get(id=self.kwargs['pk'])
+        context['product'] = product
+        context['user'] = self.request.user
+        return context
+
+class SignUpView(generic.CreateView): 
+    form_class = UserCreationForm 
+    success_url = reverse_lazy('login') 
+    template_name = 'registration/signup.html'
